cbad5b97c2f894473a4e28ecf84f0df03c02daae
Tyler Wray <wraytw@gmail.com>
2018-09-08 07:06:56 -0600

    Basic Fibonacci Number picker

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NumberPicker></NumberPicker>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NumberPicker from "./components/NumberPicker.vue";
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    NumberPicker
   }
-}
+};
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Arial, Helvetica, sans-serif;
 }
 </style>
